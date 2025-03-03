c714aaa78fa25bb2a9c2f4a4316501341daf2666
olawhales <ajaditaoreed@gmail.com>
2025-03-03 17:05:57 +0100

    updates

---
+++ src/main/java/com/example/D_CLUTTER/dto/request/users/UserChangePasswordRequest.java
@@ -0,0 +1,8 @@
+package com.example.D_CLUTTER.dto.request.users;
+
+import lombok.Data;
+
+@Data
+public class UserChangePasswordRequest {
+    public String oldPassword;
+}
--- src/main/java/com/example/D_CLUTTER/service/UserRegisterService.java
+++ src/main/java/com/example/D_CLUTTER/service/UserRegisterService.java
@@ -2,11 +2,12 @@ package com.example.D_CLUTTER.service;
 
 import com.example.D_CLUTTER.dto.Response.users.UserLoginResponse;
 import com.example.D_CLUTTER.dto.Response.users.UserRegisterResponse;
+import com.example.D_CLUTTER.dto.request.users.UserChangePasswordRequest;
 import com.example.D_CLUTTER.dto.request.users.UserLoginRequest;
 import com.example.D_CLUTTER.dto.request.users.UserRegisterRequest;
 
 public interface UserRegisterService {
     UserRegisterResponse registerUser(UserRegisterRequest userRegisterRequest);
     UserLoginResponse loginUser(UserLoginRequest userLoginRequest);
-
+//    UserChangePasswordResponse changepassword(UserChangePasswordRequest userRegisterRequest);
 }
